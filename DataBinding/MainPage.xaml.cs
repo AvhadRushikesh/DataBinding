24fef5aaaf586570ee25fd8786f6abfb684f9184
rushi-smb <rushi@smbios.co.in>
2023-02-24 18:54:51 +0530

    Add project files.

---
+++ DataBinding/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DataBinding.Models;
+
+namespace DataBinding;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		var person = new Person
+		{
+			Name = "John",
+			Phone = "9999999999",
+			Address = "X Address"
+		};
+
+		Binding personBinding = new Binding();
+
+		personBinding.Source = person;
+		personBinding.Path = "Name";
+
+		txtName.SetBinding(Label.TextProperty, personBinding);
+	}
+}
