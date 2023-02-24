04ff7a1ba0c1765167acd53d4607bf19fe58c743
rushi-smb <rushi@smbios.co.in>
2023-02-24 21:55:23 +0530

    Use INotifyPropertyChanged Interface fot Entry Binding

--- DataBinding/MainPage.xaml.cs
+++ DataBinding/MainPage.xaml.cs
@@ -4,20 +4,25 @@ namespace DataBinding;
 
 public partial class MainPage : ContentPage
 {
+	Person person = new Person();
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+        person = new Person
+        {
+            Name = "John",
+            Phone = "9999999999",
+            Address = "X Address"
+        };
+
+        BindingContext = person;
+    }
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		var person = new Person
-		{
-			Name = "John",
-			Phone = "9999999999",
-			Address = "X Address"
-		};
-
-		BindingContext = person;
+        person.Name = "Peter";
+        person.Phone = "00000";
+        person.Address = "xxxxxx";
     }
 }
--- DataBinding/Models/Person.cs
+++ DataBinding/Models/Person.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataBinding.Models
 {
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
+        private string _name;
+        private string _phone;
+        private string _address;
+        public string Name 
+        { 
+            get => _name; set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Phone
+        {
+            get => _phone; set
+            {
+                _phone = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Address 
+        { get => _address; set
+            {
+                _address = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
     }
 }
+ 
