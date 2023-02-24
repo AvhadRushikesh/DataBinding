766255a1fa0b71126c2ab7b4d8ea0609a5d6c8c5
rushi-smb <rushi@smbios.co.in>
2023-02-24 20:40:24 +0530

    Binding Between Controls

--- DataBinding/App.xaml.cs
+++ DataBinding/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DataBinding;
+﻿using DataBinding.Pages;
+
+namespace DataBinding;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new SliderPage();
 	}
 }
